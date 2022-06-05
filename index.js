03216dd1b28b021be7ea06bc494d265682b1241d
Nick Earl <nick.r.earl@gmail.com>
2022-06-04 20:59:43 -0400

    created inquierer prompt

--- index.js
+++ index.js
@@ -5,13 +5,70 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ 
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your gitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What do users need to know about contributing to this repository?',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm install',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using this repo?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license is this project using?',
+        choices: ['MIT','Apache 2.0','Mozilla Public License','GNU GPL v3','Eclipse','Boost', 'Perl', 'SIL', ]
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+}
 
-// Function call to initialize app
-init();
+
+init()
+.then(data => console.log(data))
+.catch(err =>{
+    console.log(err)
+})
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
@@ -34,7 +31,7 @@ function generateMarkdown(data) {
   ${data.usage}
 
   ## License
-  This project is licensed using the ${data.license} license. To learn more click the badge at the top of this README
+  This project is licensed using the ${data.license} license. For details look here: 
 
   ## Contributing
   ${data.contributing}
@@ -43,7 +40,7 @@ function generateMarkdown(data) {
   ${data.tests}
 
   ## Questions
-  [GitHub](https://github.com/${data.gitHub})
+  [GitHub](https://github.com/${data.github})
   email: ${data.email}
 
 `;
